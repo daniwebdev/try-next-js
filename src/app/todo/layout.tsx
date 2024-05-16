faa4e564b5dadff992b783a612d44d4e11d383a1
Dani <dani.webdev@gmail.com>
2024-05-16 21:24:11 +0700

    add todo list

---
+++ src/app/todo/layout.tsx
@@ -0,0 +1,16 @@
+export default function TodoLayout({
+    children, // will be a page or nested layout
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <section className="max-w-xl m-auto">
+        <nav>
+          <h1 className="text-3xl py-10">Todo List</h1>
+        </nav>
+   
+        {children}
+      </section>
+    )
+  }
+  
